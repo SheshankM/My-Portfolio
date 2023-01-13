0a3e65c50a1ea519ed001fb32440be1bb17e6c70
SheshankM <108621304+SheshankM@users.noreply.github.com>
2023-01-13 20:29:45 +0530

    update skill icons

---
+++ app.js
@@ -0,0 +1,32 @@
+
+let skills = {
+  html :{
+    name: `HTML`,
+    icon:`<i class="fa-brands fa-html5"></i>`
+  },
+  css :{
+    name: `CSS`,
+    icon:`<i class="fa-brands fa-css3-alt"></i>`
+  },
+  js :{
+    name: `JAVASCRIPT`,
+    icon:`<i class="fa-brands fa-js"></i>`
+  },
+  react:{
+    name: `REACTJS`,
+    icon:`<i class="fa-brands fa-react"></i>`
+  },
+  nodejs :{
+    name: `NODEJS`,
+    icon:`<i class="fa-brands fa-node"></i>`
+  },
+  expressjs :{
+    name: `EXPRESSJS`,
+    icon:`<i class="fa-brands fa-node-js"></i> `
+  }
+};
+
+// let skillEL = document.getElementById('skills-el');
+// skills.map((x)=>{
+//   skillEL.innerHTML = x.icon; 
+// })
