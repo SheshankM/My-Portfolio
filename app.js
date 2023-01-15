deeb62326ee8043de496223b4414bf8aa5b07bab
SheshankM <108621304+SheshankM@users.noreply.github.com>
2023-01-15 23:57:50 +0530

    update skills

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 
-let skills = {
-  html :{
+let skills = [
+  {
     name: `HTML`,
     icon:`<i class="fa-brands fa-html5"></i>`
   },
-  css :{
+  {
     name: `CSS`,
     icon:`<i class="fa-brands fa-css3-alt"></i>`
   },
-  js :{
+  {
     name: `JAVASCRIPT`,
     icon:`<i class="fa-brands fa-js"></i>`
   },
-  react:{
+  {
     name: `REACTJS`,
     icon:`<i class="fa-brands fa-react"></i>`
   },
-  nodejs :{
+  {
     name: `NODEJS`,
     icon:`<i class="fa-brands fa-node"></i>`
   },
-  expressjs :{
+  {
     name: `EXPRESSJS`,
     icon:`<i class="fa-brands fa-node-js"></i> `
   }
-};
+];
 
-// let skillEL = document.getElementById('skills-el');
-// skills.map((x)=>{
-//   skillEL.innerHTML = x.icon; 
-// })
+let skillEL = document.getElementById('skills-el1');
+skills.forEach((x)=>{
+  skillEL.innerHTML += x.icon; 
+})
