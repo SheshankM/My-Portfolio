6d7e54c02419c3e813a7fd81cc39eef3354eca84
SheshankM <108621304+SheshankM@users.noreply.github.com>
2023-01-18 23:13:00 +0530

    update skills view

--- app.js
+++ app.js
@@ -2,31 +2,31 @@
 let skills = [
   {
     name: `HTML`,
-    icon:`<i class="fa-brands fa-html5"></i>`
+    icon:`<i class="fa-brands fa-html5 icon"></i>`
   },
   {
     name: `CSS`,
-    icon:`<i class="fa-brands fa-css3-alt"></i>`
+    icon:`<i class="fa-brands fa-css3-alt icon"></i>`
   },
   {
     name: `JAVASCRIPT`,
-    icon:`<i class="fa-brands fa-js"></i>`
+    icon:`<i class="fa-brands fa-js icon"></i>`
   },
   {
     name: `REACTJS`,
-    icon:`<i class="fa-brands fa-react"></i>`
+    icon:`<i class="fa-brands fa-react icon"></i>`
   },
   {
     name: `NODEJS`,
-    icon:`<i class="fa-brands fa-node"></i>`
+    icon:`<i class="fa-brands fa-node icon"></i>`
   },
   {
     name: `EXPRESSJS`,
-    icon:`<i class="fa-brands fa-node-js"></i> `
+    icon:`<i class="fa-brands fa-node-js icon"></i> `
   }
 ];
 
-let skillEL = document.getElementById('skills-el1');
+let skillEL = document.getElementById('skills-el');
 skills.forEach((x)=>{
   skillEL.innerHTML += x.icon; 
 })
